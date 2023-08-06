b8914a83bc567aaba66d4b3ad2950ebb44a07283
Chander Parkash <chanderparkash179@gmail.com>
2023-08-06 16:46:43 +0500

    data binding and string interpolation

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
+
+  title: string = "List of Posts";
+
+
+
+  constructor() { }
+  ngOnInit(): void {
+  }
 
 }
