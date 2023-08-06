bf2dd2d385b5281ad763f6221174d4a79645ca68
Chander Parkash <chanderparkash179@gmail.com>
2023-08-06 17:06:42 +0500

    data binding

--- src/app/components/notes/notes.component.ts
+++ src/app/components/notes/notes.component.ts
@@ -8,4 +8,15 @@ import { Component } from '@angular/core';
 export class NotesComponent {
 
   notesTitle: string = "NOTES";
+
+  section1: string = "Intro Display Data & Event Handling";
+
+  stringInterpolationTitle: string = "String Interpolation";
+  stringInterpolationDef: string = "String Interpolation is used to show dynamic data on HTML Page or inside the view (HTML Page)";
+
+  propertyBindingTitle: string = "Property Binding";
+  propertyBindingDef: string = "";
+  propertyBindingImgUrl: string = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRg1KAnF7oK_63ml1c_JAc-PQJhnwJfCugw6Q&usqp=CAU';
+  propertyBindingImgName: string = 'property binding in angular image';
+
 }
