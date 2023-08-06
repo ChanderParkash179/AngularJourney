46ae5939727c9518948f41b7faa64c846bca301c
Chander Parkash <chanderparkash179@gmail.com>
2023-08-06 19:20:23 +0500

    removing irrelevant components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { PostComponent } from './components/post/post.component';
 import { NotesComponent } from './components/notes/notes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    PostComponent,
     NotesComponent
   ],
   imports: [
