f4cd0ca543f219b669ce0da576012fbb747eb707
Chander Parkash <chanderparkash179@gmail.com>
2023-08-11 01:58:57 +0500

    folder structure change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotesComponent } from './components/notes/notes.component';
-import { TaskOneComponent } from './components/task-one/task-one.component';
-import { DirectivesComponent } from './components/directives/directives.component';
-import { TaskTwoComponent } from './components/task-two/task-two.component';
+import { TaskOneComponent } from './components/tasks/task-one/task-one.component';
+import { DirectivesComponent } from './components/sections/directives/directives.component';
+import { TaskTwoComponent } from './components/tasks/task-two/task-two.component';
+import { PipesComponent } from './components/sections/pipes/pipes.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { TaskTwoComponent } from './components/task-two/task-two.component';
     NotesComponent,
     TaskOneComponent,
     DirectivesComponent,
-    TaskTwoComponent
+    TaskTwoComponent,
+    PipesComponent
   ],
   imports: [
     BrowserModule,
