1380f5fabb1a16af27be5dd15868d8377071ff5c
Chander Parkash <chanderparkash179@gmail.com>
2023-08-14 14:15:07 +0500

    nested form in reactive forms

--- src/app/components/sections/reactive-forms/reactive-forms.component.ts
+++ src/app/components/sections/reactive-forms/reactive-forms.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Pipe } from '@angular/core';
-import { FormGroup, FormControl, NgForm, Validators, AbstractControl } from '@angular/forms';
+import { FormGroup, FormControl, NgForm, Validators, AbstractControl, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-forms',
@@ -13,13 +13,31 @@ export class ReactiveFormsComponent implements OnInit {
   form: any = new FormGroup({
     fullName: new FormControl('', [Validators.required, Validators.minLength(6)]),
     email: new FormControl('', [Validators.required, Validators.pattern(this.emailPattern)]),
-    address: new FormControl('', [Validators.required])
+    address: new FormControl('', [Validators.required]),
+    skills: new FormArray([])
   });
+
+  get Skills() {
+    return this.form.get('skills') as FormArray;
+  }
+
+  addSkill(skill: HTMLInputElement) {
+    this.Skills.push(new FormControl(skill.value));
+
+    skill.value = '';
+  }
 
   getControl(name: any): AbstractControl | null {
     return this.form.get(name)
   }
 
+  onSubmit() {
+    console.log(this.form.value);
+  }
+
+  onRemove(index: any) {
+    this.Skills.removeAt(index);
+  }
 
   constructor() { }
 
