17a662586fe15625d947ac0a13bae9d200230ede
Chander Parkash <chanderparkash179@gmail.com>
2023-08-16 19:40:54 +0500

    get router parameter value

--- src/app/components/sections/routing-navigation/post/post.component.ts
+++ src/app/components/sections/routing-navigation/post/post.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
+
+  constructor(private _activatedRoute: ActivatedRoute) { }
+
+  id: any;
+  ngOnInit(): void {
+    this._activatedRoute.paramMap.subscribe(value => {
+      this.id = value.get('id');
+    });
+  }
 
 }
